ce48c49fdbad9740294a3c65e9ab2a60d0bff321
Harsh Bhatt <hbhatt687@gmail.com>
2017-06-18 01:19:41 -0400

    Added a second player

--- Game Tutorial/src/com/tutorial/main/Player.java
+++ Game Tutorial/src/com/tutorial/main/Player.java
@@ -11,8 +11,7 @@ public class Player extends GameObject{
 	public Player(int x, int y, ID id) {
 		super(x, y, id);
 		
-		velX = r.nextInt(5) + 1;
-		velY = r.nextInt(5);
+
 	}
 
 	
@@ -23,7 +22,11 @@ public class Player extends GameObject{
 
 	
 	public void render(Graphics g) {
-		g.setColor(Color.white);
+		if(id == ID.Player){ 
+			g.setColor(Color.white);
+		} else if (id == ID.Player2) {
+			g.setColor(Color.blue);
+		}
 		g.fillRect(x, y, 32, 32);
 		
 	}
